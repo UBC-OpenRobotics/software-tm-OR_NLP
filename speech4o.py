67ad4d0ef51cdffcbc299fad56833bc71054f82d
Devin Huang <devinhuang@server.ca>
2024-09-13 10:15:40 -0700

    Changed speechToText to obj

--- speech4o.py
+++ speech4o.py
@@ -1,18 +1,32 @@
-import speech_recognition as sr
+import speech_recognition as speech
 
-r = sr.Recognizer()
+class speechToText:
+    def convertToText():
+        r = speech.Recognizer()
 
-while(1):
-    with sr.Microphone() as source:
-        try:
-            r.adjust_for_ambient_noise(source, duration = 0.2)
-            audio = r.listen(source)
-            print("Speak now:")
-            text = r.recognize_google(audio, language = 'en-US', show_all = True)
-            print(text)
-        except sr.RequestError as e:
-            print('Could not request results')
-        
-        except sr.UnknownValueError:
-            print('unknown error occurred')
+        # Adjusts sound threshold for speech detection, 0.15 by default
+        # Values between 0 and 1, lower values means faster adjustment but may miss words
+        # r.recognizer_instance.dynamic_energy_adjustment_damping = 0.15
+
+        # Min length of silence to register end of phrase
+        # r.recognizer_instance.pause_threshold = 0.8
+
+        # Adjusts for ambient noise, then waits for noise. After that, api call to convert stt
+        while True:
+            with speech.Microphone() as source:
+                try:
+                    r.adjust_for_ambient_noise(source, duration = 0.2)
+                    audio = r.listen(source)
+                    text = r.recognize_google(audio, language = 'en-US')
+                    print(text)
+                    break
+                except speech.RequestError as e:
+                    print('Error: Bad Request')
+                
+                except speech.UnknownValueError:
+                    print('Error: Value Unknown')
+    def isCommand(self, audioInput: str):
+        pass
+
+
         
